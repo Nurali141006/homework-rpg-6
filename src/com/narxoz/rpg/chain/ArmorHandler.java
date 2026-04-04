61e24a6f9aeac99125e20742d8c31eb4d2dd5e07
Nurali Seifulla <nurali.seifulla@narxoz.kz>
2026-04-04 12:46:54 +0500

    update ArmorHandler

--- src/com/narxoz/rpg/chain/ArmorHandler.java
+++ src/com/narxoz/rpg/chain/ArmorHandler.java
@@ -10,9 +10,12 @@ public class ArmorHandler extends DefenseHandler {
     }
 
     @Override
-    public void handle(int incomingDamage, ArenaFighter target) {
-        // TODO: Subtract armorValue from incomingDamage; clamp the result to a minimum of 0.
-        // TODO: Print an armor message showing how much was absorbed.
-        // TODO: Pass the remaining damage to the next handler.
-    }
+public void handle(int incomingDamage, ArenaFighter target) {
+    int reducedDamage = Math.max(0, incomingDamage - armorValue);
+    int absorbed = incomingDamage - reducedDamage;
+
+    System.out.println("Armor absorbed " + absorbed + " damage. Remaining: " + reducedDamage);
+
+    passToNext(reducedDamage, target);
+}
 }
